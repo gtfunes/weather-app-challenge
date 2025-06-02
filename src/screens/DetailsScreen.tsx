91eabb3bce130b71ae301ffc83a790ffde435497
Gastón Funes <gtfunes@gmail.com>
2025-06-02 18:24:13 +0200

    Add forecast screen

---
+++ src/screens/DetailsScreen.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from 'react';
+import { FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { Box, HStack, Text, View, VStack } from '@gluestack-ui/themed';
+import { DateTime } from 'luxon';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import WeatherImage from '../components/ui/WeatherImage';
+
+const DetailsScreen = () => {
+  const insets = useSafeAreaInsets();
+  const forecast = useSelector((state: RootState) => state.weather.forecast);
+
+  const getDailyForecast = useCallback(() => forecast.filter((_, i) => i % 8 === 0), [forecast]);
+
+  return (
+    <View bg="$background" style={{
+        flex: 1,
+        paddingTop: insets.top + 30,
+        paddingBottom: insets.bottom,
+      }}>
+      <Box flex={1} px="$4" py="$6">
+        <FlatList
+          data={getDailyForecast()}
+          keyExtractor={(_item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Box bg="$surface" p="$3" rounded="$lg" mb="$3">
+              <HStack justifyContent="space-between">
+                <VStack space="xs">
+                  <Text bold>{DateTime.fromFormat(item.dt_txt, 'yyyy-MM-dd HH:mm:ss', { zone: 'utc' }).toLocal().toFormat('DDDD')}</Text>
+                  <Text>{item.weather[0].main}</Text>
+                  <Text>Min: {item.main.temp_min}°C</Text>
+                  <Text>Max: {item.main.temp_max}°C</Text>
+                </VStack>
+                <WeatherImage item={item.weather[0]} size={64} />
+              </HStack>
+            </Box>
+          )}
+        />
+      </Box>
+    </View>
+  );
+};
+
+export default DetailsScreen;
